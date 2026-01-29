611892654feab072e4f85f7220e9099af8f55c48
Sotiris Zegiannis <sotiriszegiannis@gmail.com>
2026-01-29 21:07:26 +0200

    done adding features reports and fixing bugs

--- Repository/DTOs/MoneyTransactionPerMonthRDTO.cs
+++ Repository/DTOs/MoneyTransactionPerMonthRDTO.cs
@@ -2,7 +2,7 @@ using Domain;
 
 namespace Repository
 {
-    public class MoneyTransactionGroupRDTO
+    public class MoneyTransactionPerMonthRDTO
     {
         public float Amount { get; set; }
         public MoneyTransactionTypeEnum Type { get; set; }
